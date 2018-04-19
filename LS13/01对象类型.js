0a6e20161d829e2ddfde2dc81c7b8931d4ff7ece
Wuchenyu6 <fan.a.fan@qq.com>
2018-04-19 16:46:59 +0800

    4.19

---
+++ LS13/01对象类型.js
@@ -0,0 +1,46 @@
+//JS对象
+var obj = {
+    num:10,
+    str:"Hi",
+    show:function(){
+        console.log(this.str);
+    }
+};
+console.log(obj.num);//10
+console.log(obj.str);//Hi
+obj.show();			 //Hi
+
+
+
+//构造器函数对象
+console.log(typeof Array);//function
+console.log(typeof Function);//function
+console.log(typeof Date);//function
+console.log(typeof Number);//function
+console.log(typeof String);//function
+console.log(typeof Boolean);//function
+//非 构造器函数 对象
+console.log(typeof Math);//object
+console.log(typeof JSON);//object
+
+
+
+//
+console.log(Object instanceof Function);//true
+console.log(Object instanceof Object);//true
+console.log(Boolean instanceof Function);//true
+console.log(Boolean instanceof Object);//true
+console.log(String instanceof Function);//true
+console.log(String instanceof Object);//true
+console.log(Number instanceof Function);//true
+console.log(Number instanceof Object);//true
+console.log(Function instanceof Function);//true
+console.log(Function instanceof Object);//true
+console.log(Array instanceof Function);//true
+console.log(Array instanceof Object);//true
+console.log(Date instanceof Function);//true
+console.log(Date instanceof Object);//true
+console.log(Math instanceof Function);//false
+console.log(Math instanceof Object);//true
+console.log(JSON instanceof Function);//false
+console.log(JSON instanceof Object);//true
